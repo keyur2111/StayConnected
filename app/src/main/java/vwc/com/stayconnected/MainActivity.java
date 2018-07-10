c0047ffbf887421572e64d31112494ed73572ce6
Keyur Sureliya <sureliya.keyur@gmail.com>
2018-07-10 15:17:03 +0530

    Permission Changes

--- app/src/main/java/vwc/com/stayconnected/MainActivity.java
+++ app/src/main/java/vwc/com/stayconnected/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -22,6 +21,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 import static android.widget.Toast.LENGTH_SHORT;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,168 +31,173 @@ public class MainActivity extends AppCompatActivity {
     private ActionBarDrawerToggle mToggle;
     private Toolbar appToolbar;
     private TabLayout tabLayout;
+    private boolean isPermissionGranted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ToolBar
-        appToolbar = (Toolbar) findViewById(R.id.app_toolbar);
-        setSupportActionBar(appToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        appToolbar.setTitle("Phone");
+        checkforPermissions();
 
-        // Side Navigation Bar
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(mToggle);
-        mToggle.syncState();
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        if(isPermissionGranted)
+        {
+            // ToolBar
+            appToolbar = (Toolbar) findViewById(R.id.app_toolbar);
+            setSupportActionBar(appToolbar);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            appToolbar.setTitle("Phone");
 
-        // Tab Layout
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-        viewPager.setOffscreenPageLimit(5);
-        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
-        viewPagerAdapter.addfragments(new Phone_Fragment(), "Phone");
-        viewPagerAdapter.addfragments(new Music_Fragment(), "Music");
-        viewPagerAdapter.addfragments(new Internal_Storage_Fragment(), "Internal Storage");
-        viewPagerAdapter.addfragments(new Gallery_Fragment(), "Gallery");
-        viewPagerAdapter.addfragments(new Messages_Fragment(), "Messages");
-        viewPager.setAdapter(viewPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-        tabLayout.setSelectedTabIndicatorColor(getColor(R.color.indigoDark));
+            // Side Navigation Bar
+            mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+            mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+            mDrawerLayout.addDrawerListener(mToggle);
+            mToggle.syncState();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-            }
+            // Tab Layout
+            tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+            ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+            viewPager.setOffscreenPageLimit(5);
+            ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+            viewPagerAdapter.addfragments(new Phone_Fragment(), "Phone");
+            viewPagerAdapter.addfragments(new Music_Fragment(), "Music");
+            viewPagerAdapter.addfragments(new Internal_Storage_Fragment(), "Internal Storage");
+            viewPagerAdapter.addfragments(new Gallery_Fragment(), "Gallery");
+            viewPagerAdapter.addfragments(new Messages_Fragment(), "Messages");
+            viewPager.setAdapter(viewPagerAdapter);
+            tabLayout.setupWithViewPager(viewPager);
+            tabLayout.setSelectedTabIndicatorColor(getColor(R.color.indigoDark));
 
-            @Override
-            public void onPageSelected(int position) {
-                if(position == 0)
-                {
-                    appToolbar.getMenu().clear();
-                    appToolbar.inflateMenu(R.menu.toolbar_menu);
-                    appToolbar.setTitle("Phone");
-                }
-                else if(position == 1)
-                {
-                    appToolbar.getMenu().clear();
-                    appToolbar.inflateMenu(R.menu.toolbar_menu);
-                    appToolbar.setTitle("Music");
-                }
-                else if(position == 2)
-                {
-                    appToolbar.getMenu().clear();
-                    appToolbar.inflateMenu(R.menu.toolbar_menu);
-                    appToolbar.setTitle("Internal Storage");
-                }
-                else if(position == 3)
-                {
-                    appToolbar.getMenu().clear();
-                    appToolbar.inflateMenu(R.menu.toolbar_menu);
-                    appToolbar.setTitle("Gallery");
-                }
-                else if(position == 4)
-                {
-                    appToolbar.getMenu().clear();
-                    appToolbar.inflateMenu(R.menu.toolbar_menu);
-                    appToolbar.setTitle("Messages");
+            viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+                @Override
+                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
                 }
 
-            }
+                @Override
+                public void onPageSelected(int position) {
+                    if(position == 0)
+                    {
+                        appToolbar.getMenu().clear();
+                        appToolbar.inflateMenu(R.menu.toolbar_menu);
+                        appToolbar.setTitle("Phone");
+                    }
+                    else if(position == 1)
+                    {
+                        appToolbar.getMenu().clear();
+                        appToolbar.inflateMenu(R.menu.toolbar_menu);
+                        appToolbar.setTitle("Music");
+                    }
+                    else if(position == 2)
+                    {
+                        appToolbar.getMenu().clear();
+                        appToolbar.inflateMenu(R.menu.toolbar_menu);
+                        appToolbar.setTitle("Internal Storage");
+                    }
+                    else if(position == 3)
+                    {
+                        appToolbar.getMenu().clear();
+                        appToolbar.inflateMenu(R.menu.toolbar_menu);
+                        appToolbar.setTitle("Gallery");
+                    }
+                    else if(position == 4)
+                    {
+                        appToolbar.getMenu().clear();
+                        appToolbar.inflateMenu(R.menu.toolbar_menu);
+                        appToolbar.setTitle("Messages");
+                    }
 
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                if (tab.getPosition() == 0)
-                {
-                    mDrawerLayout.setBackgroundResource(R.color.colorPrimaryDark);
-                    appToolbar.setBackgroundResource(R.color.colorPrimaryDark);
-                    tabLayout.setBackgroundResource(R.color.colorPrimaryDark);
-                    tabLayout.setSelectedTabIndicatorColor(getColor(R.color.indigoDark));
-                    Utils.darkenStatusBar(MainActivity.this, R.color.colorPrimaryDark);
-                    //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
                 }
-                else if (tab.getPosition() == 1)
-                {
-                    mDrawerLayout.setBackgroundResource(R.color.orange);
-                    appToolbar.setBackgroundResource(R.color.orange);
-                    tabLayout.setBackgroundResource(R.color.orange);
-                    tabLayout.setSelectedTabIndicatorColor(getColor(R.color.orangeDark));
-                    Utils.darkenStatusBar(MainActivity.this, R.color.orange);
-                    //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+
+                @Override
+                public void onPageScrollStateChanged(int state) {
+
                 }
-                else if (tab.getPosition() == 2)
-                {
-                    mDrawerLayout.setBackgroundResource(R.color.purple);
-                    appToolbar.setBackgroundResource(R.color.purple);
-                    tabLayout.setBackgroundResource(R.color.purple);
-                    tabLayout.setSelectedTabIndicatorColor(getColor(R.color.purpleDark));
-                    Utils.darkenStatusBar(MainActivity.this, R.color.purple);
-                    //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+            });
+
+            tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                @Override
+                public void onTabSelected(TabLayout.Tab tab) {
+                    if (tab.getPosition() == 0)
+                    {
+                        mDrawerLayout.setBackgroundResource(R.color.colorPrimaryDark);
+                        appToolbar.setBackgroundResource(R.color.colorPrimaryDark);
+                        tabLayout.setBackgroundResource(R.color.colorPrimaryDark);
+                        tabLayout.setSelectedTabIndicatorColor(getColor(R.color.indigoDark));
+                        Utils.darkenStatusBar(MainActivity.this, R.color.colorPrimaryDark);
+                        //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+                    }
+                    else if (tab.getPosition() == 1)
+                    {
+                        mDrawerLayout.setBackgroundResource(R.color.orange);
+                        appToolbar.setBackgroundResource(R.color.orange);
+                        tabLayout.setBackgroundResource(R.color.orange);
+                        tabLayout.setSelectedTabIndicatorColor(getColor(R.color.orangeDark));
+                        Utils.darkenStatusBar(MainActivity.this, R.color.orange);
+                        //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+                    }
+                    else if (tab.getPosition() == 2)
+                    {
+                        mDrawerLayout.setBackgroundResource(R.color.purple);
+                        appToolbar.setBackgroundResource(R.color.purple);
+                        tabLayout.setBackgroundResource(R.color.purple);
+                        tabLayout.setSelectedTabIndicatorColor(getColor(R.color.purpleDark));
+                        Utils.darkenStatusBar(MainActivity.this, R.color.purple);
+                        //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+                    }
+                    else if (tab.getPosition() == 3)
+                    {
+                        mDrawerLayout.setBackgroundResource(R.color.yellowDark);
+                        appToolbar.setBackgroundResource(R.color.yellowDark);
+                        tabLayout.setBackgroundResource(R.color.yellowDark);
+                        tabLayout.setSelectedTabIndicatorColor(getColor(R.color.orangish_yellow));
+                        Utils.darkenStatusBar(MainActivity.this, R.color.yellowDark);
+                        //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+                    }
+                    else if (tab.getPosition() == 4)
+                    {
+                        mDrawerLayout.setBackgroundResource(R.color.tomato);
+                        appToolbar.setBackgroundResource(R.color.tomato);
+                        tabLayout.setBackgroundResource(R.color.tomato);
+                        tabLayout.setSelectedTabIndicatorColor(getColor(R.color.tomatoDark));
+                        Utils.darkenStatusBar(MainActivity.this, R.color.tomato);
+                        //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+                    }
                 }
-                else if (tab.getPosition() == 3)
-                {
-                    mDrawerLayout.setBackgroundResource(R.color.yellowDark);
-                    appToolbar.setBackgroundResource(R.color.yellowDark);
-                    tabLayout.setBackgroundResource(R.color.yellowDark);
-                    tabLayout.setSelectedTabIndicatorColor(getColor(R.color.orangish_yellow));
-                    Utils.darkenStatusBar(MainActivity.this, R.color.yellowDark);
-                    //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+
+                @Override
+                public void onTabUnselected(TabLayout.Tab tab) {
+
                 }
-                else if (tab.getPosition() == 4)
-                {
-                    mDrawerLayout.setBackgroundResource(R.color.tomato);
-                    appToolbar.setBackgroundResource(R.color.tomato);
-                    tabLayout.setBackgroundResource(R.color.tomato);
-                    tabLayout.setSelectedTabIndicatorColor(getColor(R.color.tomatoDark));
-                    Utils.darkenStatusBar(MainActivity.this, R.color.tomato);
-                    //Toast.makeText(getApplicationContext(), tab.getText().toString(), LENGTH_SHORT).show();
+
+                @Override
+                public void onTabReselected(TabLayout.Tab tab) {
+
                 }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-
-
-        String[] permissions = {Manifest.permission.READ_CONTACTS, Manifest.permission.READ_CALL_LOG, Manifest.permission.WRITE_CONTACTS, Manifest.permission.WRITE_CALL_LOG, Manifest.permission.READ_EXTERNAL_STORAGE};
-
-        if (!hasPermissions(this, permissions)) {
-            ActivityCompat.requestPermissions(this, permissions, 1);
+            });
         }
+
+
     }
 
-    public static boolean hasPermissions(Context context, String... permissions)
+    public void checkforPermissions()
     {
-        if(context != null && permissions != null)
+        String[] permissions = {Manifest.permission.READ_CONTACTS,
+                Manifest.permission.READ_CALL_LOG,
+                Manifest.permission.WRITE_CONTACTS,
+                Manifest.permission.WRITE_CALL_LOG,
+                Manifest.permission.READ_EXTERNAL_STORAGE};
+
+        if(ContextCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_DENIED &&
+                ContextCompat.checkSelfPermission(this, permissions[1]) == PackageManager.PERMISSION_DENIED &&
+                ContextCompat.checkSelfPermission(this, permissions[2]) == PackageManager.PERMISSION_DENIED &&
+                ContextCompat.checkSelfPermission(this, permissions[3]) == PackageManager.PERMISSION_DENIED &&
+                ContextCompat.checkSelfPermission(this, permissions[4]) == PackageManager.PERMISSION_DENIED)
         {
-            for(String permission: permissions)
-            {
-                if(ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED)
-                {
-                    return false;
-                }
-            }
+            ActivityCompat.requestPermissions(this, permissions, 1);
+            isPermissionGranted = true;
         }
-        return true;
+
     }
 
     @Override
@@ -251,38 +257,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-
-        switch (requestCode)
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        if(grantResults[0] == -1)
         {
-            case 1:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                {
-                    if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-                    {
-                        //Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else
-                {
-                    Snackbar mySnackbar = Snackbar.make(getCurrentFocus(), "Permission Denied", Snackbar.LENGTH_SHORT);
-                    mySnackbar.setAction("Grant Access", new MyUndoListener());
-                    mySnackbar.show();
-                }
+            isPermissionGranted = false;
+            Toast.makeText(this, "Please Grant Permission to Continue.", Toast.LENGTH_LONG).show();
+            Snackbar mySnackbar = Snackbar.make(findViewById(R.id.relative_layout_main_activity), "Permission Denied", Snackbar.LENGTH_INDEFINITE);
+            mySnackbar.setAction("Grant Access", new MyUndoListener());
+            mySnackbar.show();
         }
     }
 
-    public class MyUndoListener implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-
-            if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            {
-                ActivityCompat.requestPermissions(getParent(),new String[] {Manifest.permission.READ_EXTERNAL_STORAGE }, 1);
-            }
-        }
-    }
+    public class MyUndoListener implements View.OnClickListener{ @Override public void onClick(View v) { checkforPermissions(); }}
 
     public static class Utils {
 
